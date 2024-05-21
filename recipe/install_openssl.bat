004ca0a1aba1ec738e4b71c313eee4c32c848aba
Isuru Fernando <isuruf@gmail.com>
2024-05-21 00:55:27 -0500

    Fix installing activate scripts

--- recipe/install_openssl.bat
+++ recipe/install_openssl.bat
@@ -31,3 +31,13 @@ rem copy out32dll\ssleay32.dll %LIBRARY_BIN%\ssleay32.dll
 rem copy out32dll\libeay32.dll %LIBRARY_BIN%\libeay32.dll
 rem mkdir %LIBRARY_INC%\openssl
 rem xcopy /S inc32\openssl\*.* %LIBRARY_INC%\openssl\
+
+:: Copy the [de]activate scripts to %PREFIX%\etc\conda\[de]activate.d.
+:: This will allow them to be run on environment activation.
+for %%F in (activate deactivate) DO (
+    if not exist %PREFIX%\etc\conda\%%F.d mkdir %PREFIX%\etc\conda\%%F.d
+    copy "%RECIPE_DIR%\%%F-win.bat" "%PREFIX%\etc\conda\%%F.d\%PKG_NAME%_%%F-win.bat"
+    copy "%RECIPE_DIR%\%%F-win.ps1" "%PREFIX%\etc\conda\%%F.d\%PKG_NAME%_%%F-win.ps1"
+    :: Copy unix shell activation scripts, needed by Windows Bash users
+    copy "%RECIPE_DIR%\%%F-win.sh" "%PREFIX%\etc\conda\%%F.d\%PKG_NAME%_%%F-win.sh"
+)
